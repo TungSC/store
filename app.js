3a2df9fab7a8cfa6e876c8c76bbe61c946df8f78
TungSC <tungngo@gempages.help>
2022-03-28 23:44:35 +0700

    refactor init app

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const dotenv = require('dotenv');
+import createError from 'http-errors'
+import express from 'express';
+import path from "path";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-dotenv.config()
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const app = express();
+import index from "./routes/index.js";
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+class App {
+  constructor() {
+    this.app = express()
+    this.paths = {
+      homepage: "/",
+    }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    this.middlewares();
+    this.routes();
+    this.initModule();
+  }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  initModule() {
+    this.app.use(function(req, res, next) {
+      next(createError(404));
+    });
+    this.app.use(this.errorHandler)
+    this.app.set('views', path.join(__dirname, 'views'));
+    this.app.set('view engine', 'ejs');
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, 'public')));
+  }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  middlewares() {
+    this.app.use(cors()); // Enable CORS
+  }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Bind controllers to routes
+  routes() {
+    this.app.use(this.paths.homepage, index);
+    //this.app.use(this.paths.homepage, require("./routes/users"));
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  errorHandler(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app;
+    res.status(err.status || 500);
+    res.render('error');
+  }
+
+  listen() {
+      console.log("Server running on port: ", process.env.PORT);
+  }
+}
+
+export default App
---
+++ bin/www.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+import dotenv from 'dotenv';
+dotenv.config()
+
+import http from 'http';
+import App from '../app.js';
+const app = new App();
+
+const server = http.createServer(app.app);
+server.listen(process.env.PORT);
+server.on('listening', app.listen);
+server.on('error', onError);
